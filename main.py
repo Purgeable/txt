258647a628deabaf5d68fdc2b9e1b6d963d80f63
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-02-09 02:40:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,44 @@
 Simple notepad.exe notes parser. 
 
 Aims to convert .txt file to a datastructure of tasks 
-with subtasks/timestamps. Similar in spririt to 
-*todotxt.com*
-
-TODO: - see todo1 and todo 2 in code below
-      - splitting newly added code to testable/'does 
-        one thing' functions highly encoraged 
-        
-NOT TODO: - read ordered dict from yaml
+with subtasks/timestamps. Similar in spririt to *todotxt.com*
 """
 
 import yaml
 from datetime import datetime
 import re
 
+doc1 = """worktitle:
+- subtask description 23:30 06.02.2016
+- 23:30 06.02.2016 23:36 06.02.2016 subtask description 2
+- [s] 23:30 06.02.2016 subtask description 3"""
+
+doc2 = """worktitle
+    subtask description 23:30 06.02.2016
+    23:30 06.02.2016 23:36 06.02.2016 subtask description 2
+    [s] 23:30 06.02.2016 subtask description 3"""
+#rules for parsing doc2:
+#    text starting with no offset (at start of string) is title
+#    four spaces offset is subtask line 
+#    single letter in square brackets anywhere in subtask line is status flag 
+#    one timestamp is 'last checked' date and time 
+#    two timestamps is 'started' and 'ended' date and time 
+#    remaining text after popping out status and timestamp(s) is 'desc'
+
+subtask_dict_sample = {'status':'s', 
+'last checked': None, 
+'started': datetime(2016, 2, 6, 23, 30), 
+'ended': datetime(2016, 2, 9, 2, 30),
+'desc': 'subtask description 3'}
+
+assert subtask_dict_sample == parse_subtask('[s] 23:30 06.02.2016 ... 2:30 09.02.2016 subtask description 3') 
+assert subtask_dict_sample == parse_subtask('[s] 23:30 06.02.2016 2:30 09.02.2016 subtask description 3')
+assert subtask_dict_sample == parse_subtask('[s] subtask description 3 23:30 06.02.2016 2:30 09.02.2016')
 
 ref_yaml_parsed = {'worktitle': [   
-  ['', '23:30 06.02.2016', '', 'subtask description 1']
-, ['', '23:30 06.02.2016', '23:36 06.02.2016', 'subtask description 2']
-, ['', '23:30 06.02.2016', '', 'subtask description 3']
+  [ '', '23:30 06.02.2016', '', 'subtask description 1']
+, [ '', '23:30 06.02.2016', '23:36 06.02.2016', 'subtask description 2']
+, ['s', '23:30 06.02.2016', '', 'subtask description 3']
 ]}
 
 
@@ -36,12 +55,6 @@ def subtask_to_list(s):
 
 # for reference
 # print (yaml.dump(ref_yaml_parsed))
-
-doc1 = """worktitle:
-- subtask description 23:30 06.02.2016
-- 23:30 06.02.2016 23:36 06.02.2016 subtask description 2
-- [s] 23:30 06.02.2016 subtask description 3"""
-
 
 def parse_yml(string):
     parsed = yaml.parse(doc1)
