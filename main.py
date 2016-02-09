2336140a72fed3532ddc780efc7b7b7415b7d8e9
Artyom Veremeenko <arwer13@gmail.com>
2016-02-09 23:30:59 +0300

    Added parsing of subtask with 1 or 2 dates and status.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import yaml
 from datetime import datetime
 import re
 
+
 doc1 = """worktitle:
 - subtask description 23:30 06.02.2016
 - 23:30 06.02.2016 23:36 06.02.2016 subtask description 2
@@ -33,7 +34,43 @@ subtask_dict_sample = {'status':'s',
 'ended': datetime(2016, 2, 9, 2, 30),
 'desc': 'subtask description 3'}
 
-assert subtask_dict_sample == parse_subtask('[s] 23:30 06.02.2016 ... 2:30 09.02.2016 subtask description 3') 
+ref_dt = datetime(2016, 2, 6, 23, 30)
+
+def ts_to_datetime(timestamp):
+    return datetime.strptime(timestamp, '%H:%M %d.%m.%Y')
+
+assert ref_dt == ts_to_datetime('23:30 06.02.2016')
+
+
+def parse_subtask(s):
+    result = {
+        'status': None,
+        'last checked': None,
+        'started': None,
+        'ended': None,
+        'desc': ''
+    }
+    datetime_re = re.compile(r'([012]?\d:\d\d [0123]\d\.[01]\d\.\d\d\d\d)')
+    status_re = re.compile(r'\[\s*(\w+)\s*\]')
+
+    status_list = status_re.findall(s)
+    if len(status_list) > 0:
+        result['status'] = status_list[0]
+
+    datetime_list = map(ts_to_datetime, datetime_re.findall(s))
+    if len(datetime_list) == 1:
+        result['last checked'] = datetime_list[0]
+    elif len(datetime_list) == 2:
+        result['started'] = datetime_list[0]
+        result['ended'] = datetime_list[1]
+
+    s = datetime_re.sub('', s)
+    s = status_re.sub('', s)
+    result['desc'] = s.strip()
+    return result
+
+
+assert subtask_dict_sample == parse_subtask('[s] 23:30 06.02.2016 ... 2:30 09.02.2016 subtask description 3')
 assert subtask_dict_sample == parse_subtask('[s] 23:30 06.02.2016 2:30 09.02.2016 subtask description 3')
 assert subtask_dict_sample == parse_subtask('[s] subtask description 3 23:30 06.02.2016 2:30 09.02.2016')
 
@@ -44,30 +81,16 @@ ref_yaml_parsed = {'worktitle': [
 ]}
 
 
-def subtask_to_list(s):
-    result = ['']
-    dt_pattern = r'[012]\d:\d\d [0123]\d\.[01]\d\.\d\d\d\d'
-    result += re.findall(dt_pattern, s)
-    result += [''] if len(result) == 2 else []
-    result.append(re.sub(dt_pattern, '', s, count=2).strip())
-    return result
-
-
 # for reference
 # print (yaml.dump(ref_yaml_parsed))
 
-def parse_yml(string):
-    parsed = yaml.parse(doc1)
-    for k, v in parsed.iteritems():
-        if type(v) == list:
-            parsed[k] = subtask_to_list(v)
-    return parsed
+# def parse_yml(string):
+#     parsed = yaml.parse(doc1)
+#     for k, v in parsed.iteritems():
+#         if type(v) == list:
+#             parsed[k] = subtask_to_list(v)
+#     return parsed
 
 # assert parse_yml(doc1) == ref_yaml_parsed
 
-ref_dt = datetime(2016, 2, 6, 23, 30)
 
-def ts_to_datetime(timestamp):
-    return datetime.strptime(timestamp, '%H:%M %d.%m.%Y')
-
-assert ref_dt == ts_to_datetime('23:30 06.02.2016')
