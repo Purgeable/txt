0313c40af7044b280683d7c70b426d6dd66a3e1a
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-02-11 13:02:12 +0300

    update with todos for #1

--- main.py
+++ main.py
@@ -1,46 +1,70 @@
 # -*- coding: utf-8 -*-
 """
-Simple notepad.exe notes parser. 
-
-Aims to convert .txt file to a datastructure of tasks 
-with subtasks/timestamps. Similar in spririt to *todotxt.com*
+Simple windows notepad.exe todo notes parser 
+ - makes use of F5 timestamp in notepad.exe: 23:30 06.02.2016
+ - aims to convert .txt file to a datastructure of tasks with subtasks/timestamps. 
+ - similar in spririt to http://todotxt.com/
 """
 
 import yaml
 from datetime import datetime
 import re
 
-
-doc1 = """worktitle:
-- subtask description 23:30 06.02.2016
-- 23:30 06.02.2016 23:36 06.02.2016 subtask description 2
-- [s] 23:30 06.02.2016 subtask description 3"""
-
 doc2 = """worktitle
     subtask description 23:30 06.02.2016
     23:30 06.02.2016 23:36 06.02.2016 subtask description 2
     [s] 23:30 06.02.2016 subtask description 3"""
+    
 #rules for parsing doc2:
 #    text starting with no offset (at start of string) is title
 #    four spaces offset is subtask line 
-#    single letter in square brackets anywhere in subtask line is status flag 
+
+# parsing subtasks 
+#    single letter in square brackets anywhere in subtask line is status flag + only one [x] is taken 
 #    one timestamp is 'last checked' date and time 
 #    two timestamps is 'started' and 'ended' date and time 
 #    remaining text after popping out status and timestamp(s) is 'desc'
 
-subtask_dict_sample = {'status':'s', 
+subtask_dict_sample = {'status': 's', 
 'last checked': None, 
-'started': datetime(2016, 2, 6, 23, 30), 
-'ended': datetime(2016, 2, 9, 2, 30),
-'desc': 'subtask description 3'}
-
-ref_dt = datetime(2016, 2, 6, 23, 30)
+'started':      datetime(2016, 2, 6, 23, 30), 
+'ended':        datetime(2016, 2, 9, 2, 30),
+'desc':         'subtask description 3'}
 
 def ts_to_datetime(timestamp):
     return datetime.strptime(timestamp, '%H:%M %d.%m.%Y')
 
-assert ref_dt == ts_to_datetime('23:30 06.02.2016')
+assert ts_to_datetime('23:30 06.02.2016') == datetime(2016, 2, 6, 23, 30)
 
+# catches one letter inside brackets, like '[d]'
+STATUS_PAT = re.compile(r'\[\s*(\w+)\s*\]')
+
+# catches notepad.exe F5 timestamp
+DATETIME_PAT = re.compile(r'([012]?\d:\d\d [0123]\d\.[01]\d\.\d\d\d\d)')
+    
+def get_status_letter(string, status_re = STATUS_PAT):
+    {
+    status_list = status_re.findall(string)
+    if len(status_list) > 0:
+        result['status'] = status_list[0]
+        }
+ 
+ # note will skip [x] after first occurence
+ assert get_status_letter('[s] [z]') = 's'
+ # todo - ниже правильно?
+ assert get_status_letter('[abc]') is None
+ 
+def get_datetime_list(string, datetime_re = DATETIME_PAT):
+    return map(ts_to_datetime, datetime_re.findall(string))
+
+# todo - пару assert'ов здесь
+
+def get_description(s, status_re = STATUS_PAT, datetime_re = DATETIME_PAT):
+    s = datetime_re.sub('', s)
+    s = status_re.sub('', s)
+    return s.strip()
+
+# todo - пару assert'ов здесь
 
 def parse_subtask(s):
     result = {
@@ -48,37 +72,41 @@ def parse_subtask(s):
         'last checked': None,
         'started': None,
         'ended': None,
-        'desc': ''
+        'desc': '' # EP: not sure it is not None
     }
-    datetime_re = re.compile(r'([012]?\d:\d\d [0123]\d\.[01]\d\.\d\d\d\d)')
-    status_re = re.compile(r'\[\s*(\w+)\s*\]')
-
-    status_list = status_re.findall(s)
-    if len(status_list) > 0:
-        result['status'] = status_list[0]
-
-    datetime_list = map(ts_to_datetime, datetime_re.findall(s))
+    
+    # get status letter like 's' from '[s]' 
+    result['status'] = get_status_letter(s)
+    
+    # get timestamps, allocate to dictionary 
+    datetime_list = get_datetime_list(s)
     if len(datetime_list) == 1:
         result['last checked'] = datetime_list[0]
     elif len(datetime_list) == 2:
         result['started'] = datetime_list[0]
         result['ended'] = datetime_list[1]
-
-    s = datetime_re.sub('', s)
-    s = status_re.sub('', s)
-    result['desc'] = s.strip()
+        
+    # allocate description 
+    result['desc'] = get_description(s)
+   
     return result
-
 
 assert subtask_dict_sample == parse_subtask('[s] 23:30 06.02.2016 ... 2:30 09.02.2016 subtask description 3')
 assert subtask_dict_sample == parse_subtask('[s] 23:30 06.02.2016 2:30 09.02.2016 subtask description 3')
 assert subtask_dict_sample == parse_subtask('[s] subtask description 3 23:30 06.02.2016 2:30 09.02.2016')
+# more whitespace
+assert subtask_dict_sample == parse_subtask('[s]    subtask description 3     23:30 06.02.2016    2:30 09.02.2016   ')
 
-ref_yaml_parsed = {'worktitle': [   
-  [ '', '23:30 06.02.2016', '', 'subtask description 1']
-, [ '', '23:30 06.02.2016', '23:36 06.02.2016', 'subtask description 2']
-, ['s', '23:30 06.02.2016', '', 'subtask description 3']
-]}
+
+##############
+# May delete #
+##############
+
+#ref_yaml_parsed = {'worktitle': [   
+#  [ '', '23:30 06.02.2016', '', 'subtask description 1']
+#, [ '', '23:30 06.02.2016', '23:36 06.02.2016', 'subtask description 2']
+#, ['s', '23:30 06.02.2016', '', 'subtask description 3']
+#]}
 
 
 # for reference
@@ -94,3 +122,7 @@ ref_yaml_parsed = {'worktitle': [
 # assert parse_yml(doc1) == ref_yaml_parsed
 
 
+#doc1 = """worktitle:
+#- subtask description 23:30 06.02.2016
+#- 23:30 06.02.2016 23:36 06.02.2016 subtask description 2
+#- [s] 23:30 06.02.2016 subtask description 3"""
